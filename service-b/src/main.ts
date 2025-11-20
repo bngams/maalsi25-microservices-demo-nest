d2a0856d308fcfb2243ea385ee69c6372ab4e62b
Boris Ngam <boris@deltavia.com>
2025-11-20 15:38:22 +0100

    microservices basic config

--- service-b/src/main.ts
+++ service-b/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Transport } from '@nestjs/microservices/enums/transport.enum';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: 'localhost',
+      port: 3002,
+    },
+  });
+  await app.listen();
 }
 bootstrap();
